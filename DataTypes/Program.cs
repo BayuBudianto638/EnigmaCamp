b81cf773af3e03e4921304f8e2993de3aab082ea
Bayu <radamanthys_ok@yahoo.com>
2022-08-26 15:05:10 +0700

    added how to avoid if

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -274,29 +274,69 @@
 
 // C# program to illustrate the
 // use of goto in switch statement
-using System;
-public class EnigmaCamp
-{
-    // Main Method
-    public static void Main(String[] args)
-    {
-        int greeting = 2;
+//using System;
+//public class EnigmaCamp
+//{
+//    // Main Method
+//    public static void Main(String[] args)
+//    {
+//        int greeting = 2;
 
-        switch (greeting)
-        {
-            case 1:
-                Console.WriteLine("Hello");
-                goto default;
-            case 2:
-                Console.WriteLine("Bonjour");
-                goto case 3;
-            case 3:
-                Console.WriteLine("Namaste");
-                goto default;
-            default:
-                Console.WriteLine("Entered value is: " + greeting);
-                break;
-        }
-    }
-}
+//        switch (greeting)
+//        {
+//            case 1:
+//                Console.WriteLine("Hello");
+//                goto default;
+//            case 2:
+//                Console.WriteLine("Bonjour");
+//                goto case 3;
+//            case 3:
+//                Console.WriteLine("Namaste");
+//                goto default;
+//            default:
+//                Console.WriteLine("Entered value is: " + greeting);
+//                break;
+//        }
+//    }
+//}
 
+//How to avoid if
+var a = new int[] { 1, 2, 3, 4, 5 };
+
+var foundInt = a.FirstOrDefault(w => w == 4);
+
+Console.WriteLine($"{foundInt.ToString()}");
+Console.ReadKey();
+
+var foundIntSingle = a.Single(w => w == 4);
+
+Console.WriteLine($"{foundIntSingle.ToString()}");
+Console.ReadKey();
+
+
+string monthName = "Juni";
+
+var tableMonth = new string[] { "Bulan", "Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember" };
+var listMonth = tableMonth.ToList();
+int month = listMonth.IndexOf(monthName);
+Console.WriteLine($"{month.ToString()}");
+Console.ReadKey();
+
+
+if (month == 1) Console.WriteLine($"{31}");
+if (month == 2) Console.WriteLine($"{29}");
+if (month == 3) Console.WriteLine($"{31}");
+if (month == 4) Console.WriteLine($"{30}");
+if (month == 5) Console.WriteLine($"{31}");
+if (month == 6) Console.WriteLine($"{30}");
+if (month == 7) Console.WriteLine($"{31}");
+if (month == 8) Console.WriteLine($"{31}");
+if (month == 9) Console.WriteLine($"{30}");
+if (month == 10) Console.WriteLine($"{31}");
+if (month == 11) Console.WriteLine($"{30}");
+if (month == 12) Console.WriteLine($"{31}");
+
+var monthDays= new int[] { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+Console.WriteLine($"{monthDays[month - 1]}");
+
+Console.ReadKey();
