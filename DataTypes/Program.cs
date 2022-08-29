1f35182bd4c9eec0afa23885989fd0c19c7e9189
Bayu <Bayu.Budianto@nttindonesia.com>
2022-08-29 09:49:17 +0700

    commited

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -96,22 +96,135 @@
 //    }
 //}
 
-// C# program to illustrate the
-// use of break statement
+//// C# program to illustrate the
+//// use of break statement
+//using System;
+//class EnigmaCamp
+//{
+//    // Main Method
+//    static public void Main()
+//    {
+//        // EnigmaCamp is printed only 2 times
+//        // because of break statement
+//        for (int i = 1; i < 4; i++)
+//        {
+//            if (i == 3)
+//                break;
+
+//            Console.WriteLine("EnigmaCamp");
+//        }
+//    }
+//}
+
+//// C# program to illustrate the
+//// use of continue statement
+//using System;
+
+//class EnigmaCamp
+//{
+//	// Main Method
+//	public static void Main()
+//	{
+//		// This will skip 4 to print
+//		for (int i = 1; i <= 10; i++)
+//		{
+//			// if the value of i becomes 4 then
+//			// it will skip 4 and send the
+//			// transfer to the for loop and
+//			// continue with 5
+//			if (i == 4)
+//				continue;
+
+//			Console.WriteLine(i);
+//		}
+//	}
+//}
+
+//// C# program to illustrate the
+//// use of goto statement
+//using System;
+//class EnigmaCamp
+//{
+//	// Main Method
+//	static public void Main()
+//	{
+//		int number = 20;
+//		switch (number)
+//		{
+//			case 5:
+//				Console.WriteLine("case 5");
+//				break;
+//			case 10:
+//				Console.WriteLine("case 10");
+//				break;
+//			case 20:
+//				Console.WriteLine("case 20");
+
+//				// goto statement transfer
+//				// the control to case 5
+//				goto case 5;
+
+//			default:
+//				Console.WriteLine("No match found");
+//				break;
+//		}
+//	}
+//}
+
+//// C# program to illustrate the
+//// use of return statement
+//using System;
+//class EnigmaCamp
+//{
+//	// creating simple addition function
+//	static int Addition(int a)
+//	{
+//		// add two value and
+//		// return the result of addition
+//		int add = a + a;
+
+//		// using return statement
+//		return add;
+//	}
+//	// Main Method
+//	static public void Main()
+//	{
+//		int number = 2;
+
+//		// calling addition function
+//		int result = Addition(number);
+//		Console.WriteLine("The addition is {0}", result);
+//	}
+//}
+
+// C# Program to illustrate the use
+// of throw keyword
 using System;
 class EnigmaCamp
 {
-    // Main Method
-    static public void Main()
-    {
-        // EnigmaCamp is printed only 2 times
-        // because of break statement
-        for (int i = 1; i < 4; i++)
-        {
-            if (i == 3)
-                break;
+	// taking null in the string
+	static string sub = null;
+	// method to display subject name
+	static void displaysubject(string sub1)
+	{
+		if (sub1 == null)
+			throw new NullReferenceException("Exception Message");
+	}
 
-            Console.WriteLine("EnigmaCamp");
-        }
-    }
+	// Main Method
+	static void Main(string[] args)
+	{
+		// using try catch block to
+		// handle the Exception
+		try
+		{
+			// calling the static method
+			displaysubject(sub);
+		}
+
+		catch (Exception exp)
+		{
+			Console.WriteLine(exp.Message);
+		}
+	}
 }
