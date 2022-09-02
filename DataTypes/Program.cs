1876721e709a94cf6e11b929db62083260c1dcba
Bayu <Bayu.Budianto@nttindonesia.com>
2022-09-02 14:46:38 +0700

    commited

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,23 +1,22 @@
-﻿//// Contoh 1
-//using DataTypes;
-//using DataTypes.Model;
-//class program
-//{
-//    static void Main()
-//    {
-//        using (var context = new SchoolContext())
-//        {
-//            var student = new Student()
-//            {
-//                StudentId = 1,
-//                Name = "Anton Medan"
-//            };
+﻿// Contoh 1
+using DataTypes;
+using DataTypes.Model;
+class program
+{
+    static void Main()
+    {
+        using (var context = new SchoolContext())
+        {
+            var student = new Student()
+            {
+                Name = "Anton Medan"
+            };
 
-//            context.Students.Add(student);
-//            context.SaveChanges();
-//        }
-//    }
-//}
+            context.Students.Add(student);
+            context.SaveChanges();
+        }
+    }
+}
 
 // Contoh 2 menggunakan Interface, class service dan Dependency Injection
 //using DataTypes.Interface;
@@ -39,52 +38,52 @@
 
 
 // Contoh 3 Using Menu like basic Pascal/C++ Console
-class Program
-{
-    static void Main(string[] args)
-    {
-        bool showMenu = true;
-        while (showMenu)
-        {
-            showMenu = MainMenu();
-        }
-    }
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        bool showMenu = true;
+//        while (showMenu)
+//        {
+//            showMenu = MainMenu();
+//        }
+//    }
 
-    private static bool MainMenu()
-    {
-        Console.Clear();
-        Console.WriteLine("Choose an option:");
-        Console.WriteLine("1) Create Student");
-        Console.WriteLine("2) Update Student");
-        Console.WriteLine("3) Exit");
-        Console.Write("\r\nSelect an option: ");
+//    private static bool MainMenu()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Choose an option:");
+//        Console.WriteLine("1) Create Student");
+//        Console.WriteLine("2) Update Student");
+//        Console.WriteLine("3) Exit");
+//        Console.Write("\r\nSelect an option: ");
 
-        switch (Console.ReadLine())
-        {
-            case "1":
-                CreateStudent();
-                return true;
-            case "2":
-                UpdateStudent();
-                return true;
-            case "3":
-                return false;
-            default:
-                return true;
-        }
-    }
+//        switch (Console.ReadLine())
+//        {
+//            case "1":
+//                CreateStudent();
+//                return true;
+//            case "2":
+//                UpdateStudent();
+//                return true;
+//            case "3":
+//                return false;
+//            default:
+//                return true;
+//        }
+//    }
 
-    private static void CreateStudent()
-    {
-        Console.Clear();
-        Console.WriteLine("Create Student");
-        Console.ReadKey();
-    }
+//    private static void CreateStudent()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Create Student");
+//        Console.ReadKey();
+//    }
 
-    private static void UpdateStudent()
-    {
-        Console.Clear();
-        Console.WriteLine("Update Student");
-        Console.ReadKey();
-    }
-}
+//    private static void UpdateStudent()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Update Student");
+//        Console.ReadKey();
+//    }
+//}
