89420537265a68234c9774dff6d3c744a26213b3
Bayu <Bayu.Budianto@nttindonesia.com>
2022-09-02 16:33:07 +0700

    Example Solid Principles

---
+++ EnigmaCore/Interface/IStudent.cs
@@ -0,0 +1,17 @@
+﻿using EnigmaCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaCore.Interface
+{
+    public interface IStudent
+    {
+        void SaveStudent(StudentModel student);
+        void UpdateStudent(StudentModel student);
+        void DeleteStudent(int studentId);
+        List<StudentModel> GetAllStudent();
+    }
+}
---
+++ EnigmaService/DefaultService/StudentService.cs
@@ -0,0 +1,33 @@
+﻿using EnigmaCore.Interface;
+using EnigmaCore.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnigmaService.DefaultService
+{
+    public class StudentService : IStudent
+    {
+        public void DeleteStudent(int studentId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StudentModel> GetAllStudent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveStudent(StudentModel student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStudent(StudentModel student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
