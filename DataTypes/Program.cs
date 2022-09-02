033b6d3299c8a19c82197afaa446634aef387207
Bayu <Bayu.Budianto@nttindonesia.com>
2022-09-02 16:46:37 +0700

    MainMenu Commited

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -10,6 +10,51 @@ class Program
             .AddSingleton<IStudent, StudentService>()
             .BuildServiceProvider();
 
+        bool showMenu = true;
+        while (showMenu)
+        {
+            showMenu = MainMenu();
+        }
+    }
 
+    private static bool MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("SOLID Principles Implementation");
+        Console.WriteLine("-------------------------------");
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1) Create Student");
+        Console.WriteLine("2) Update Student");
+        Console.WriteLine("3) Exit");
+        Console.Write("\r\nSelect an option: ");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                CreateStudent();
+                return true;
+            case "2":
+                UpdateStudent();
+                return true;
+            case "3":
+                return false;
+            default:
+                return true;
+        }
+    }
+
+    private static void CreateStudent()
+    {
+        Console.Clear();
+        Console.WriteLine("Create Student");
+        Console.ReadKey();
+    }
+
+    private static void UpdateStudent()
+    {
+        Console.Clear();
+        Console.WriteLine("Update Student");
+        Console.ReadKey();
     }
 }
+
