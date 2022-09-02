da9cdc19ffdc0e86fed6c13061ed1d08acbd8f99
Bayu <Bayu.Budianto@nttindonesia.com>
2022-09-02 09:42:56 +0700

    commited

---
+++ DataTypes/Mecha.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypes
+{
+    public abstract class Mecha
+    {
+        public abstract void BomNuklir();
+        public abstract void MataLaser();
+    }
+}
--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,32 +1,131 @@
-﻿// CONTOH 1
-// Abstract class
-abstract class Animal
+﻿//// CONTOH 1
+//// Abstract class
+//abstract class Animal
+//{
+//    // Abstract method (does not have a body)
+//    public abstract void animalSound();
+//    // Regular method
+//    public void sleep()
+//    {
+//        Console.WriteLine("Zzz");
+//    }
+//}
+
+//// Derived class (inherit from Animal)
+//class Pig : Animal
+//{
+//    public override void animalSound()
+//    {
+//        // The body of animalSound() is provided here
+//        Console.WriteLine("The pig says: wee wee");
+//    }
+//}
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        Pig myPig = new Pig(); // Create a Pig object
+//        myPig.animalSound();  // Call the abstract method
+//        myPig.sleep();  // Call the regular method
+//    }
+//}
+
+//// CONTOH 2
+//public abstract class Monster
+//{
+//    public abstract void Tidur();
+//}
+
+//public class Kaiju : Monster
+//{
+//    public override void Tidur()
+//    {
+//        Console.WriteLine("Kaiju Tidur");
+//    }
+//}
+
+//public class Godzilla : Monster
+//{
+//    public override void Tidur()
+//    {
+//        Console.WriteLine("Godzilla Tidur");
+//    }
+
+//    public void AtomicBreath()
+//    {
+//        Console.WriteLine("Godzilla Atomic Breath");
+//    }
+//}
+
+//class program
+//{
+//    static void Main(string[] args)
+//    {
+//        Kaiju kaiju = new Kaiju();
+//        kaiju.Tidur();
+
+//        Godzilla godzilla = new Godzilla();
+//        godzilla.Tidur();
+//        godzilla.AtomicBreath();
+
+//        Console.ReadKey();
+//    }
+//}
+
+// CONTOH 3
+using DataTypes;
+
+public class GurrenLagann : Mecha
 {
-    // Abstract method (does not have a body)
-    public abstract void animalSound();
-    // Regular method
-    public void sleep()
+    public override void BomNuklir()
     {
-        Console.WriteLine("Zzz");
+        Console.WriteLine("TGGL Bom Nuklir");
+    }
+
+    public override void MataLaser()
+    {
+        Console.WriteLine("TGGL Mengeluarkan mata laser");
+    }
+
+    public void GigaDrill()
+    {
+        Console.WriteLine("TGGL GigaDrill");
     }
 }
 
-// Derived class (inherit from Animal)
-class Pig : Animal
+public class Patlabor : Mecha
 {
-    public override void animalSound()
+    public override void BomNuklir()
     {
-        // The body of animalSound() is provided here
-        Console.WriteLine("The pig says: wee wee");
+        Console.WriteLine("Patlabor tidak memiliki bom nuklir");
+    }
+
+    public override void MataLaser()
+    {
+        Console.WriteLine("Patlabor tidak memiliki mata laser");
+    }
+
+    public void PistolRevolver()
+    {
+        Console.WriteLine("Patlabor mengeluarkan pistol revolver");
     }
 }
 
-class Program
+class program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Pig myPig = new Pig(); // Create a Pig object
-        myPig.animalSound();  // Call the abstract method
-        myPig.sleep();  // Call the regular method
+        GurrenLagann gl = new GurrenLagann();
+        gl.BomNuklir();
+        gl.MataLaser();
+        gl.GigaDrill();
+
+        Patlabor pl = new Patlabor();
+        pl.BomNuklir();
+        pl.MataLaser();
+        pl.PistolRevolver();
+
+        Console.ReadKey();
     }
 }
