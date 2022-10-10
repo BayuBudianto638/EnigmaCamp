abc1699aabf0faf56b9c60367e1072892cbbc8ba
Bayu <Bayu.Budianto@nttindonesia.com>
2022-10-10 14:47:54 +0700

    commited

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -458,3 +458,161 @@
 //        }
 //    }
 //}
+
+//Contoh 3 Using Menu like basic Pascal/C++ Console
+using DataTypes.Model;
+using DataTypes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DataTypes.Interface;
+using DataTypes.Service;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddLogging()
+            .AddSingleton<IStudent, StudentService>()
+            .BuildServiceProvider();
+
+        bool showMenu = true;
+        while (showMenu)
+        {
+            showMenu = MainMenu();
+        }
+    }
+
+    private static bool MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1) Create Student");
+        Console.WriteLine("2) Update Student");
+        Console.WriteLine("3) Get Student By Id");
+        Console.WriteLine("4) Get All Student");
+        Console.WriteLine("5) Exit");
+        Console.Write("\r\nSelect an option: ");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                CreateStudent();
+                return true;
+            case "2":
+                UpdateStudent();
+                return true;
+            case "3":
+                GetStudentById();
+                return true;
+            case "4":
+                GetAllStudent();
+                return true;
+            case "5":
+                return false;
+            default:
+                return true;
+        }
+    }
+
+    private static void CreateStudent()
+    {
+        Console.Clear();
+        Console.WriteLine("Create Student");
+        Console.WriteLine("---------------");
+
+        Console.Write("Name:");
+        string nama = Console.ReadLine();
+        Console.Write("Address:");
+        string address = Console.ReadLine();
+        Console.Write("Country:");
+        string country = Console.ReadLine();
+
+        var student = new Student();
+        student.Name = nama;
+        student.Address = address;
+        student.Country = country;
+
+        var studentService = new StudentService();
+        studentService.SimpanStudent(student);
+
+        Console.ReadKey();
+    }
+
+    private static void UpdateStudent()
+    {
+        Console.Clear();
+        Console.WriteLine("Update Student");
+        Console.WriteLine("---------------");
+        Console.Write("Cari Id:");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        var studentService = new StudentService();
+        var student = studentService.GetById(id);
+
+        if (student != null)
+        {
+            Console.WriteLine($"Nama : {student.Name}");
+            Console.WriteLine($"Address : {student.Address}");
+            Console.WriteLine($"Country : {student.Country}");
+
+            Console.WriteLine("---------------");
+            Console.ReadKey();
+
+            Console.Write("Name:");
+            string nama = Console.ReadLine();
+            Console.Write("Address:");
+            string address = Console.ReadLine();
+            Console.Write("Country:");
+            string country = Console.ReadLine();
+
+            student.StudentId = student.StudentId;
+            student.Name = nama;
+            student.Address = address;
+            student.Country = country;
+
+            studentService.UpdateStudent(student);
+        }
+        else
+        {
+            Console.WriteLine("Id tidak ditemukan");
+        }
+
+        Console.ReadKey();
+    }
+
+    private static void GetStudentById()
+    {
+        Console.Clear();
+        Console.WriteLine("Get Student By Id");
+        Console.WriteLine("---------------");
+        Console.Write("Id:");
+        int id = Convert.ToInt32(Console.ReadLine());
+
+        var studentService = new StudentService();
+        var student = studentService.GetById(id);
+
+        Console.WriteLine($"Nama : {student.Name}");
+        Console.WriteLine($"Address : {student.Address}");
+        Console.WriteLine($"Country : {student.Country}");
+
+        Console.ReadKey();
+    }
+
+    private static void GetAllStudent()
+    {
+        Console.Clear();
+        Console.WriteLine("Get All Student");
+        Console.WriteLine("---------------");
+
+        var studentService = new StudentService();
+        var studentList = studentService.GetAllStudents();
+
+        foreach (var item in studentList)
+        {
+            Console.WriteLine($"{item.StudentId} - {item.Name} - {item.Address} - {item.Country}");
+        }
+
+        Console.ReadKey();
+    }
+}
