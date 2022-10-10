aed17e5ee9c2d6caf1c3e5322e3ea28124757d58
Bayu <Bayu.Budianto@nttindonesia.com>
2022-10-10 13:31:48 +0700

    commited

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -58,46 +58,6 @@
 //    }
 //}
 
-using System;
-using System.IO;
-
-class MyStream
-{
-    private const string FILE_NAME = "Test.data";
-
-    public static void Main()
-    {
-        if (File.Exists(FILE_NAME))
-        {
-            Console.WriteLine($"{FILE_NAME} already exists!");
-            return;
-        }
-
-        using (FileStream fs = new FileStream(FILE_NAME, FileMode.CreateNew))
-        {
-            using (BinaryWriter w = new BinaryWriter(fs))
-            {
-                for (int i = 0; i < 11; i++)
-                {
-                    w.Write(i);
-                }
-            }
-        }
-
-        using (FileStream fs = new FileStream(FILE_NAME, FileMode.Open, FileAccess.Read))
-        {
-            using (BinaryReader r = new BinaryReader(fs))
-            {
-                for (int i = 0; i < 11; i++)
-                {
-                    Console.WriteLine(r.ReadInt32());
-                }
-            }
-        }
-    }
-}
-
-
 // The example creates a file named "Test.data" and writes the integers 0 through 10 to it in binary format.
 // It then writes the contents of Test.data to the console with each integer on a separate line.
 
@@ -426,77 +386,75 @@ class MyStream
 ////    }
 ////}
 
-//// CONTOH 9
-////// DATASET
-////using System;
-////using System.Data.SqlClient;
-////using System.Data;
-////namespace DataSetExample
-////{
-////    public partial class DataSetDemo : System.Web.UI.Page
-////    {
-////        protected void Page_Load(object sender, EventArgs e)
-////        {
-////            using (SqlConnection con = new SqlConnection("data source=.; database=student; integrated security=SSPI"))
-////            {
-////                SqlDataAdapter sde = new SqlDataAdapter("Select * from student", con);
-////                DataSet ds = new DataSet();
-////                sde.Fill(ds);
-////                GridView1.DataSource = ds;
-////                GridView1.DataBind();
-////            }
-////        }
-////    }
-////}
+////CONTOH 9
+////DATASET
+//using System;
+//using System.Data.SqlClient;
+//using System.Data;
+//namespace DataSetExample
+//{
+//    class sqlDataAdapter
+//    {
+//        static void Main()
+//        {
+//            using (SqlConnection con = new SqlConnection("data source=.; database=student; integrated security=SSPI"))
+//            {
+//                SqlDataAdapter sde = new SqlDataAdapter("Select * from student", con);
+//                DataSet ds = new DataSet();
+//                sde.Fill(ds);
 
-//// CONTOH 10
-//// DataAdapter
-////using System;
-////using System.Data.SqlClient;
-////using System.Data;
-////namespace DataSetExample
-////{
-////    public partial class DataSetDemo : System.Web.UI.Page
-////    {
-////        protected void Page_Load(object sender, EventArgs e)
-////        {
-////            using (SqlConnection con = new SqlConnection("data source=.; database=student; integrated security=SSPI"))
-////            {
-////                SqlDataAdapter sde = new SqlDataAdapter("Select * from student", con);
-////                DataSet ds = new DataSet();
-////                sde.Fill(ds);
-////                GridView1.DataSource = ds;
-////                GridView1.DataBind();
-////            }
-////        }
-////    }
-////}
+//                Console.WriteLine("Using Data Table");
+//                foreach (DataRow row in ds.Tables["student"].Rows)
+//                {
+//                    Console.WriteLine(row["StudentId"] + ",  " + row["Name"] + ",  " + row["Address"] + ",  " + row["Country"]);
+//                }
+//            }
+//        }
+//    }
+//}
 
-////// CONTOH11
-////// DATATABLE
-////using System;
-////using System.Collections.Generic;
-////using System.Data;
-////using System.Linq;
-////using System.Web;
-////using System.Web.UI;
-////using System.Web.UI.WebControls;
-////namespace DataTableDemo
-////{
-////    public partial class DataTableForm : System.Web.UI.Page
-////    {
-////        protected void Page_Load(object sender, EventArgs e)
-////        {
-////            DataTable table = new DataTable();
-////            table.Columns.Add("ID");
-////            table.Columns.Add("Name");
-////            table.Columns.Add("Email");
-////            table.Rows.Add("101", "Rameez", "rameez@example.com");
-////            table.Rows.Add("102", "Sam Nicolus", "sam@example.com");
-////            table.Rows.Add("103", "Subramanium", "subramanium@example.com");
-////            table.Rows.Add("104", "Ankur Kumar", "ankur@example.com");
-////            GridView1.DataSource = table;
-////            GridView1.DataBind();
-////        }
-////    }
-////}
+// CONTOH11
+// DATATABLE
+using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace AdoNetConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string ConString = "data source=.; database=StudentDB; integrated security=SSPI";
+                using (SqlConnection connection = new SqlConnection(ConString))
+                {
+                    SqlDataAdapter da = new SqlDataAdapter("select * from students", connection);
+
+                    //Using Data Table
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    Console.WriteLine("Using Data Table");
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        Console.WriteLine(row["StudentId"] + ",  " + row["Name"] + ",  " + row["Address"] + ",  " + row["Country"]);
+                    }
+                    Console.WriteLine("---------------");
+                    //Using DataSet
+                    DataSet ds = new DataSet();
+                    da.Fill(ds, "student");
+                    Console.WriteLine("Using Data Set");
+                    foreach (DataRow row in ds.Tables["students"].Rows)
+                    {
+                        Console.WriteLine(row["StudentId"] + ",  " + row["Name"] + ",  " + row["Address"] + ",  " + row["Country"]);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong.\n" + e);
+            }
+            Console.ReadKey();
+        }
+    }
+}
