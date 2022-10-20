b32dd31e9c3037a252827b01874e294af8277621
Bayu <Bayu.Budianto@nttindonesia.com>
2022-10-20 11:27:56 +0700

    commited

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -61,50 +61,90 @@
 // The example creates a file named "Test.data" and writes the integers 0 through 10 to it in binary format.
 // It then writes the contents of Test.data to the console with each integer on a separate line.
 
+// CONTOH 1
+// CREATE DATABASE VIA ADO.NET
+//using System;
+//using System.Data.SqlClient;
+//using System.Xml.Linq;
 
-////// CONTOH 1
-////// CREATE DATABASE VIA ADO.NET
-////using System;
-////using System.Data.SqlClient;
-////using System.Xml.Linq;
+//namespace EnigmaCampADONetConsole
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            new Program().ConnectToDb();
+//        }
+//        public void ConnectToDb()
+//        {
+//            SqlConnection con = null;
+//            try
+//            {
+//                // Creating Connection  
+//                con = new SqlConnection("data source=---NAMA LOCALHOST---; database=student; integrated security=SSPI");
 
-////namespace EnigmaCampADONetConsole
-////{
-////    class Program
-////    {
-////        static void Main(string[] args)
-////        {
-////            new Program().CreateTable();
-////        }
-////        public void CreateTable()
-////        {
-////            SqlConnection con = null;
-////            try
-////            {
-////                // Creating Connection  
-////                con = new SqlConnection("data source=---NAMA LOCALHOST---; database=student; integrated security=SSPI");
-////                // writing sql query  
-////                SqlCommand cm = new SqlCommand(@"create table student(studentId int not null,   
-////                name varchar(100))", con);  
-////                // Opening Connection  
-////                con.Open();
-////                // Executing the SQL query  
-////                cm.ExecuteNonQuery();
-////                // Displaying a message  
-////                Console.WriteLine("Table created Successfully");
-////            }
-////            catch (Exception e)
-////            {
-////                Console.WriteLine("OOPs, something went wrong." + e);
-////            }
-////            // Closing the connection  
-////            finally
-////            {
-////                con.Close();
-////            }
-////        }
-////    }
-////}
+//                // Opening Connection  
+//                con.Open();
+//                // Displaying a message  
+//                Console.WriteLine("Connect Successfully");
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("OOPs, something went wrong." + e);
+//            }
+//            // Closing the connection  
+//            finally
+//            {
+//                con.Close();
+//            }
+//        }
+//    }
+//}
+
+
+//// CONTOH 1
+//// CREATE DATABASE VIA ADO.NET
+//using System;
+//using System.Data.SqlClient;
+//using System.Xml.Linq;
+
+//namespace EnigmaCampADONetConsole
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            new Program().CreateTable();
+//        }
+//        public void CreateTable()
+//        {
+//            SqlConnection con = null;
+//            try
+//            {
+//                // Creating Connection  
+//                con = new SqlConnection("data source=---NAMA LOCALHOST---; database=student; integrated security=SSPI");
+//                // writing sql query  
+//                SqlCommand cm = new SqlCommand(@"create table student(studentId int not null,   
+//                name varchar(100))", con);
+//                // Opening Connection  
+//                con.Open();
+//                // Executing the SQL query  
+//                cm.ExecuteNonQuery();
+//                // Displaying a message  
+//                Console.WriteLine("Table created Successfully");
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("OOPs, something went wrong." + e);
+//            }
+//            // Closing the connection  
+//            finally
+//            {
+//                con.Close();
+//            }
+//        }
+//    }
+//}
 
 
 //// CONTOH 2
@@ -190,6 +230,44 @@
 //            catch (Exception e)
 //            {
 //                sqlTransaction.Rollback();
+//                Console.WriteLine("OOPs, something went wrong.\n" + e);
+//            }
+//            // Closing the connection  
+//            finally
+//            {
+//                con.Close();
+//            }
+//        }
+//    }
+//}
+
+//using System;
+//using System.Data.SqlClient;
+//namespace AdoNetConsoleApplication
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            new Program().CreateTable();
+//        }
+//        public void CreateTable()
+//        {
+//            SqlConnection con = null;
+//            try
+//            {
+//                // Creating Connection  
+//                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+//                // writing sql query  
+//                SqlCommand cm = new SqlCommand(@"update student set name = 'Anton Medan' where id = '101'", con);
+//                // Opening Connection  
+//                con.Open();
+//                // Executing the SQL query  
+//                cm.ExecuteNonQuery();
+//                Console.WriteLine("Record Updated Successfully");
+//            }
+//            catch (Exception e)
+//            {
 //                Console.WriteLine("OOPs, something went wrong.\n" + e);
 //            }
 //            // Closing the connection  
@@ -386,8 +464,8 @@
 ////    }
 ////}
 
-////CONTOH 9
-////DATASET
+//CONTOH 9
+//DATASET
 //using System;
 //using System.Data.SqlClient;
 //using System.Data;
@@ -457,162 +535,272 @@
 //            Console.ReadKey();
 //        }
 //    }
+////}
+
+////Contoh 3 Using Menu like basic Pascal/C++ Console
+//using DataTypes.Model;
+//using DataTypes;
+//using Microsoft.EntityFrameworkCore;
+//using Microsoft.Extensions.DependencyInjection;
+//using DataTypes.Interface;
+//using DataTypes.Service;
+
+//class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        var serviceProvider = new ServiceCollection()
+//            .AddLogging()
+//            .AddSingleton<IStudent, StudentService>()
+//            .BuildServiceProvider();
+
+//        bool showMenu = true;
+//        while (showMenu)
+//        {
+//            showMenu = MainMenu();
+//        }
+//    }
+
+//    private static bool MainMenu()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Choose an option:");
+//        Console.WriteLine("1) Create Student");
+//        Console.WriteLine("2) Update Student");
+//        Console.WriteLine("3) Get Student By Id");
+//        Console.WriteLine("4) Get All Student");
+//        Console.WriteLine("5) Exit");
+//        Console.Write("\r\nSelect an option: ");
+
+//        switch (Console.ReadLine())
+//        {
+//            case "1":
+//                CreateStudent();
+//                return true;
+//            case "2":
+//                UpdateStudent();
+//                return true;
+//            case "3":
+//                GetStudentById();
+//                return true;
+//            case "4":
+//                GetAllStudent();
+//                return true;
+//            case "5":
+//                return false;
+//            default:
+//                return true;
+//        }
+//    }
+
+//    private static void CreateStudent()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Create Student");
+//        Console.WriteLine("---------------");
+
+//        Console.Write("Name:");
+//        string nama = Console.ReadLine();
+//        Console.Write("Address:");
+//        string address = Console.ReadLine();
+//        Console.Write("Country:");
+//        string country = Console.ReadLine();
+
+//        var student = new Student();
+//        student.Name = nama;
+//        student.Address = address;
+//        student.Country = country;
+
+//        var studentService = new StudentService();
+//        studentService.SimpanStudent(student);
+
+//        Console.ReadKey();
+//    }
+
+//    private static void UpdateStudent()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Update Student");
+//        Console.WriteLine("---------------");
+//        Console.Write("Cari Id:");
+//        int id = Convert.ToInt32(Console.ReadLine());
+
+//        var studentService = new StudentService();
+//        var student = studentService.GetById(id);
+
+//        if (student != null)
+//        {
+//            Console.WriteLine($"Nama : {student.Name}");
+//            Console.WriteLine($"Address : {student.Address}");
+//            Console.WriteLine($"Country : {student.Country}");
+
+//            Console.WriteLine("---------------");
+//            Console.ReadKey();
+
+//            Console.Write("Name:");
+//            string nama = Console.ReadLine();
+//            Console.Write("Address:");
+//            string address = Console.ReadLine();
+//            Console.Write("Country:");
+//            string country = Console.ReadLine();
+
+//            student.StudentId = student.StudentId;
+//            student.Name = nama;
+//            student.Address = address;
+//            student.Country = country;
+
+//            studentService.UpdateStudent(student);
+//        }
+//        else
+//        {
+//            Console.WriteLine("Id tidak ditemukan");
+//        }
+
+//        Console.ReadKey();
+//    }
+
+//    private static void GetStudentById()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Get Student By Id");
+//        Console.WriteLine("---------------");
+//        Console.Write("Id:");
+//        int id = Convert.ToInt32(Console.ReadLine());
+
+//        var studentService = new StudentService();
+//        var student = studentService.GetById(id);
+
+//        Console.WriteLine($"Nama : {student.Name}");
+//        Console.WriteLine($"Address : {student.Address}");
+//        Console.WriteLine($"Country : {student.Country}");
+
+//        Console.ReadKey();
+//    }
+
+//    private static void GetAllStudent()
+//    {
+//        Console.Clear();
+//        Console.WriteLine("Get All Student");
+//        Console.WriteLine("---------------");
+
+//        var studentService = new StudentService();
+//        var studentList = studentService.GetAllStudents();
+
+//        foreach (var item in studentList)
+//        {
+//            Console.WriteLine($"{item.StudentId} - {item.Name} - {item.Address} - {item.Country}");
+//        }
+
+//        Console.ReadKey();
+//    }
 //}
 
-//Contoh 3 Using Menu like basic Pascal/C++ Console
-using DataTypes.Model;
-using DataTypes;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using DataTypes.Interface;
-using DataTypes.Service;
+// ADO DATA READER
+//using System;
+//using System.Data.SqlClient;
+//namespace AdoNetConsoleApplication
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            new Program().GetData();
+//        }
+//        public void GetData()
+//        {
+//            SqlConnection con = null;
+//            try
+//            {
+//                // Creating Connection  
+//                con = new SqlConnection("data source=.; database=student; integrated security=SSPI");
+//                // writing sql query  
+//                SqlCommand cm = new SqlCommand("select * from student", con);
+//                // Opening Connection  
+//                con.Open();
+//                // Executing the SQL query  
+//                SqlDataReader sdr = cm.ExecuteReader();
+//                while (sdr.Read())
+//                {
+//                    Console.WriteLine(sdr["name"] + " " + sdr["email"]);
+//                }
+//            }
+//            catch (Exception e)
+//            {
+//                Console.WriteLine("OOPs, something went wrong." + e);
+//            }
+//            // Closing the connection  
+//            finally
+//            {
+//                con.Close();
+//            }
+//        }
+//    }
+//}
 
-class Program
+// TRANSACTION
+using System;
+using System.Data.SqlClient;
+namespace ADOTransactionsDemo
 {
-    static void Main(string[] args)
+    class Program
     {
-        var serviceProvider = new ServiceCollection()
-            .AddLogging()
-            .AddSingleton<IStudent, StudentService>()
-            .BuildServiceProvider();
-
-        bool showMenu = true;
-        while (showMenu)
+        public static string ConnectionString = "data source=LAPTOP-ICA2LCQL\\SQLEXPRESS; initial catalog=ADODB; integrated security=True";
+        static void Main(string[] args)
         {
-            showMenu = MainMenu();
-        }
-    }
-
-    private static bool MainMenu()
-    {
-        Console.Clear();
-        Console.WriteLine("Choose an option:");
-        Console.WriteLine("1) Create Student");
-        Console.WriteLine("2) Update Student");
-        Console.WriteLine("3) Get Student By Id");
-        Console.WriteLine("4) Get All Student");
-        Console.WriteLine("5) Exit");
-        Console.Write("\r\nSelect an option: ");
-
-        switch (Console.ReadLine())
-        {
-            case "1":
-                CreateStudent();
-                return true;
-            case "2":
-                UpdateStudent();
-                return true;
-            case "3":
-                GetStudentById();
-                return true;
-            case "4":
-                GetAllStudent();
-                return true;
-            case "5":
-                return false;
-            default:
-                return true;
-        }
-    }
-
-    private static void CreateStudent()
-    {
-        Console.Clear();
-        Console.WriteLine("Create Student");
-        Console.WriteLine("---------------");
-
-        Console.Write("Name:");
-        string nama = Console.ReadLine();
-        Console.Write("Address:");
-        string address = Console.ReadLine();
-        Console.Write("Country:");
-        string country = Console.ReadLine();
-
-        var student = new Student();
-        student.Name = nama;
-        student.Address = address;
-        student.Country = country;
-
-        var studentService = new StudentService();
-        studentService.SimpanStudent(student);
-
-        Console.ReadKey();
-    }
-
-    private static void UpdateStudent()
-    {
-        Console.Clear();
-        Console.WriteLine("Update Student");
-        Console.WriteLine("---------------");
-        Console.Write("Cari Id:");
-        int id = Convert.ToInt32(Console.ReadLine());
-
-        var studentService = new StudentService();
-        var student = studentService.GetById(id);
-
-        if (student != null)
-        {
-            Console.WriteLine($"Nama : {student.Name}");
-            Console.WriteLine($"Address : {student.Address}");
-            Console.WriteLine($"Country : {student.Country}");
-
-            Console.WriteLine("---------------");
+            try
+            {
+                Console.WriteLine("Before Transaction");
+                GetAccountsData();
+                MoneyTransfer();
+                Console.WriteLine("After Transaction");
+                GetAccountsData();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("OOPs, something went wrong" + e.Message);
+            }
             Console.ReadKey();
-
-            Console.Write("Name:");
-            string nama = Console.ReadLine();
-            Console.Write("Address:");
-            string address = Console.ReadLine();
-            Console.Write("Country:");
-            string country = Console.ReadLine();
-
-            student.StudentId = student.StudentId;
-            student.Name = nama;
-            student.Address = address;
-            student.Country = country;
-
-            studentService.UpdateStudent(student);
         }
-        else
+        private static void MoneyTransfer()
         {
-            Console.WriteLine("Id tidak ditemukan");
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                // The connection needs to be open before we begin a transaction
+                connection.Open();
+                // Create the transaction object by calling the BeginTransaction method on connection object
+                SqlTransaction transaction = connection.BeginTransaction();
+                try
+                {
+                    // Associate the first update command with the transaction
+                    SqlCommand cmd = new SqlCommand("UPDATE Accounts SET Balance = Balance - 500 WHERE AccountNumber = 'Account1'", connection, transaction);
+                    cmd.ExecuteNonQuery();
+                    // Associate the second update command with the transaction
+                    cmd = new SqlCommand("UPDATE Accounts SET Balance = Balance + 500 WHERE AccountNumber = 'Account2'", connection, transaction);
+                    cmd.ExecuteNonQuery();
+                    // If everythinhg goes well then commit the transaction
+                    transaction.Commit();
+                    Console.WriteLine("Transaction Committed");
+                }
+                catch
+                {
+                    // If anything goes wrong, rollback the transaction
+                    transaction.Rollback();
+                    Console.WriteLine("Transaction Rollback");
+                }
+            }
         }
-
-        Console.ReadKey();
-    }
-
-    private static void GetStudentById()
-    {
-        Console.Clear();
-        Console.WriteLine("Get Student By Id");
-        Console.WriteLine("---------------");
-        Console.Write("Id:");
-        int id = Convert.ToInt32(Console.ReadLine());
-
-        var studentService = new StudentService();
-        var student = studentService.GetById(id);
-
-        Console.WriteLine($"Nama : {student.Name}");
-        Console.WriteLine($"Address : {student.Address}");
-        Console.WriteLine($"Country : {student.Country}");
-
-        Console.ReadKey();
-    }
-
-    private static void GetAllStudent()
-    {
-        Console.Clear();
-        Console.WriteLine("Get All Student");
-        Console.WriteLine("---------------");
-
-        var studentService = new StudentService();
-        var studentList = studentService.GetAllStudents();
-
-        foreach (var item in studentList)
+        private static void GetAccountsData()
         {
-            Console.WriteLine($"{item.StudentId} - {item.Name} - {item.Address} - {item.Country}");
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("Select * from Accounts", connection);
+                SqlDataReader sdr = cmd.ExecuteReader();
+                while (sdr.Read())
+                {
+                    Console.WriteLine(sdr["AccountNumber"] + ",  " + sdr["CustomerName"] + ",  " + sdr["Balance"]);
+                }
+            }
         }
-
-        Console.ReadKey();
     }
 }
