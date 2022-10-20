5281f9f8625a2e308bdc0684aa2f22d5e89ba6a1
Bayu <Bayu.Budianto@nttindonesia.com>
2022-10-20 15:42:28 +0700

    commited

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,2 +1,29 @@
-﻿Console.WriteLine("Hello World!");
-Console.ReadKey();
+﻿using System.Data.SqlClient;
+
+var sql = "select * from products";
+var products = new List<Product>();
+using (var connection = new SqlConnection(connString))
+{
+    connection.Open();
+    using (var command = new SqlCommand(sql, connection))
+    {
+        using (var reader = command.ExecuteReader())
+        {
+            var product = new Product
+            {
+                ProductId = reader.GetInt32(reader.GetOrdinal("ProductId")),
+                ProductName = reader.GetString(reader.GetOrdinal("ProductName")),
+                SupplierId = reader.GetInt32(reader.GetOrdinal("SupplierId")),
+                CategoryId = reader.GetInt32(reader.GetOrdinal("CategoryId")),
+                QuantityPerUnit = reader.GetString(reader.GetOrdinal("QuantityPerUnit")),   
+                UnitPrice = reader.GetDecimal(reader.GetOrdinal("UnitPrice")),
+                UnitsInStock = reader.GetInt16(reader.GetOrdinal("UnitsInStock")),
+                UnitsOnOrder = reader.GetInt16(reader.GetOrdinal("UnitsOnOrder")),
+                ReorderLevel = reader.GetInt16(reader.GetOrdinal("ReorderLevel")),
+                Discontinued = reader.GetBoolean(reader.GetOrdinal("Discontinued")),
+                DiscontinuedDate = reader.GetDateTime(reader.GetOrdinal("DiscontinuedDate"))
+            };
+            products.Add(product);
+        }
+    }
+}
