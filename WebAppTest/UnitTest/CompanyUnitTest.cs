14715dedbf9150642541583433acf6bfc644677a
bayuhyuuga <radamanthys_ok@yahoo.com>
2022-12-05 13:28:13 +0700

    company staged

---
+++ WebAppTest/UnitTest/CompanyUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAppCore.Interface;
+using WebAppCore.Model;
+
+namespace WebAppTest.UnitTest
+{
+    public class CompanyUnitTest
+    {
+        [Fact]
+        public void CreateCompany()
+        {
+            var companyService = new Mock<ICompanyAppService>();
+
+            var newCompany = new CompanyDto();
+            newCompany.Id = 1;
+            newCompany.Name = "Apple";
+            newCompany.Address = "Oregon";
+            newCompany.Country = "USA";
+
+            var result = companyService.Setup(service => service.Create(newCompany)).Returns(newCompany);
+
+            Assert.NotNull(result);
+        }
+    }
+}
